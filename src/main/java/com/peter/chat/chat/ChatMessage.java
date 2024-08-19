4541f4c6dd9f788379feade6b68248a6995cd3a1
peterhuang569 <peterzdhuang@gmail.com>
2024-08-18 22:25:40 -0600

    init

---
+++ src/main/java/com/peter/chat/chat/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.peter.chat.chat;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage{
+
+    private String content;
+    private String sender;
+    private MessageType messageType;
+
+}
---
+++ src/main/java/com/peter/chat/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.peter.chat.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
