77a23d5341cfc2c9b497c70b4ff290433170e92b
peterhuang569 <peterzdhuang@gmail.com>
2024-08-20 22:35:41 -0600

    fixed the issue, the application was not dockerizing properly, now it is posted on dockerhub

--- src/main/java/com/peter/chat/ChatApplication.java
+++ src/main/java/com/peter/chat/ChatApplication.java
@@ -3,10 +3,8 @@ package com.peter.chat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.peter.chat.chat")
 public class ChatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
