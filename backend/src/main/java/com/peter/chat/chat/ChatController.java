0b26fdb7729d89e41c1d3d401e1503fdf5865392
peterhuang569 <peterzdhuang@gmail.com>
2024-09-21 21:44:31 -0600

    configured the ports

--- backend/src/main/java/com/peter/chat/chat/ChatController.java
+++ backend/src/main/java/com/peter/chat/chat/ChatController.java
@@ -9,7 +9,6 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Controller
 public class ChatController {
